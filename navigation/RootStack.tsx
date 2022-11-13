dfc4ecd48069f4383648500ad1e3cb078464ea89
codono <codono@github.com>
2022-11-13 15:43:59 +0900

    implement context value into navigator

--- navigation/RootStack.tsx
+++ navigation/RootStack.tsx
@@ -1,14 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useContext} from 'react';
 import {Home} from '../pages/Home';
+import {Context} from '../provider/RootProvider';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack(): React.ReactElement {
+  const {organizationName, repositoryName} = useContext(Context);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="home">
+      <Stack.Navigator
+        initialRouteName="home"
+        screenOptions={{title: `${organizationName} / ${repositoryName}`}}>
         <Stack.Screen name="home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
