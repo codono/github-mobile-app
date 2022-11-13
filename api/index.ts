049990b10fe1e074b1bc4154353466591e1ec545
codono <codono@github.com>
2022-11-13 16:00:50 +0900

    feat getIssueList, getIssueDetail api

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+const GIT_REPO_API_URL = 'https://api.github.com/repos';
+
+export const getIssueList = async (owner: string, repoName: string) => {
+  try {
+    const result = await fetch(
+      `${GIT_REPO_API_URL}/${owner}/${repoName}/issues`,
+    );
+    const resultJson = await result.json();
+    return resultJson; // returns {result:url}
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getIssueDetail = async (
+  owner: string,
+  repoName: string,
+  issueNumber: number,
+) => {
+  try {
+    const result = await fetch(
+      `${GIT_REPO_API_URL}/${owner}/${repoName}/issues/${issueNumber}`,
+    );
+    const resultJson = await result.json();
+    return resultJson; // returns {result:url}
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
